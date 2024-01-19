c94eb0fa1b9e95b7b295a1f361a0bde8ce69f0b7
rares06 <151221476+rares06@users.noreply.github.com>
2024-01-20 00:45:54 +0200

    Add files via upload

--- proiect_web/js/signin.js
+++ proiect_web/js/signin.js
@@ -1,1 +1,13 @@
+let users = [];
+var email = document.getElementById('email-input').textContent;
+var password = document.getElementById('password-input').textContent;
 
+function signin() {
+    users.push(email, password);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    users.forEach((user) => {
+        console.log(user.email, user.password);
+    });
+}
